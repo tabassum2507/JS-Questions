701e1cde2b15cecde345bfa67ff0ce74d627ecfb
tabassum2507 <10tabassum2@gmail.com>
2021-12-04 23:57:20 +0530

    D-1

---
+++ maxValue.js
@@ -0,0 +1,14 @@
+const value1 = 1500;
+const value2 = 900;
+const value3 = 1600;
+if (value1 > value2){
+if (value1 > value3){
+console.log(“value1 is bigger”)
+}
+else{
+console.log(“value3 is bigger”)
+}
+}
+else {
+console.log(“value2 is bigger”)
+}
