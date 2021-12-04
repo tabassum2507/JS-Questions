f5d5f045705ad32d85b1c32aca1909713dc9656c
tabassum2507 <10tabassum2@gmail.com>
2021-12-04 23:54:05 +0530

    D-1

---
+++ fibonacci-recursion.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+    if (n == 0) {
+    return [0];
+    }
+    if (n == 1) {
+    return [0, 1];
+    }
+    else {
+    let fibo = fibonacci(n-1);
+    let element = fibo(n-1) + fibo(n-2);
+    fibo.push(element)
+    return fibo;
+    }
+}
+    
