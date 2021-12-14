0837898b8ad456ecf6021bed567ecd618c43480e
tabassum2507 <10tabassum2@gmail.com>
2021-12-14 23:46:59 +0530

    D-6

---
+++ number.js
@@ -0,0 +1,4 @@
+const validateNumber = n => !isNaN(parseFloat(n)) && isFinite(n) && Number(n) == n;
+console.log(validateNumber('200'));
+console.log(validateNumber('10'));
+console.log(validateNumber('abc'));
