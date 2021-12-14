ba2cbeb5740227a48783edfce46b7fd4bdd68379
tabassum2507 <10tabassum2@gmail.com>
2021-12-14 23:51:59 +0530

    D-6

---
+++ suffix.js
@@ -0,0 +1,16 @@
+const toOrdinalSuffix = num => {
+    const int = parseInt(num),
+      digits = [int % 10, int % 100],
+      ordinals = ['st', 'nd', 'rd', 'th'],
+      oPattern = [1, 2, 3, 4],
+      tPattern = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+    return oPattern.includes(digits[0]) && !tPattern.includes(digits[1])
+      ? int + ordinals[digits[0] - 1]
+      : int + ordinals[3];
+  };
+  
+  console.log(toOrdinalSuffix('1')); 
+  console.log(toOrdinalSuffix('4')); 
+  console.log(toOrdinalSuffix('50')); 
+  console.log(toOrdinalSuffix('123'));
+  
