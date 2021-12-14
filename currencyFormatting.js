9f67944358928b8d36b855bc361c0d296914f6ff
tabassum2507 <10tabassum2@gmail.com>
2021-12-14 23:55:34 +0530

    D-6

---
+++ currencyFormatting.js
@@ -0,0 +1,8 @@
+const toCurrency = (n, curr, LanguageFormat = undefined) =>
+  Intl.NumberFormat(LanguageFormat, { style: 'currency', currency: curr }).format(n);
+
+console.log(toCurrency(123456.789, 'EUR')); // currency: Euro | currencyLangFormat: Local
+console.log(toCurrency(123456.789, 'USD', 'en-us')); // currency: US Dollar | currencyLangFormat: English (United States)
+console.log(toCurrency(123456.789, 'USD', 'fa')); //currency: US Dollar | currencyLangFormat: Farsi
+console.log(toCurrency(322342436423.2435, 'JPY')); //currency: Japanese Yen | currencyLangFormat: Local
+console.log(toCurrency(322342436423.2435, 'JPY', 'fi')); //currency: Japanese Yen | currencyLangFormat: Finnish
